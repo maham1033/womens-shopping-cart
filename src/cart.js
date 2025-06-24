eab55277c6df2b16f6081ed29fe8ab6b05797715
Maham Nadeem <109579257+maham1033@users.noreply.github.com>
2025-06-24 14:39:23 +0500

    Add files via upload

---
+++ src/cart.js
@@ -0,0 +1,132 @@
+let label=document.getElementById('label');
+let ShoppingCart=document.getElementById('shopping-cart');
+let basket=JSON.parse(localStorage.getItem("data")) || [];
+console.log(basket);
+
+let calculation=()=>{
+let carticon=document.getElementById("cartAmount");
+carticon.innerHTML=basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
+
+
+}
+calculation();
+
+let generateCartItems=()=>{
+    if (basket.length !==0) {
+        return ShoppingCart.innerHTML=basket.map((x)=>{
+            console.log(x);
+            let {id,item}=x;
+            let search=shopItemsData.find((y)=>y.id === id) || [];
+            let {img,name,price}=search;
+            return  `
+        <div class="cart-item">
+    <img width="100" src="${img}" alt="">
+    <div class="details">
+    <div class="title-price-x">
+    <h4 class="title-price">
+    <p>${name}</p>
+    <p class="cart-item-price">$ ${price}</p>
+    </h4>
+    <i onclick="removeItem('${id}')" class="bi bi-x-lg"></i>
+    </div>
+     <div class="buttons">
+                        <i onclick="decrement('${id}')" class="bi bi-dash-lg"></i>
+                        <div id="${id}" class="quantity">
+                        ${item}
+                        </div>
+                        <i onclick="increment('${id}')" class="bi bi-plus-lg"></i>
+                    </div>
+    <h3> $ ${item*search.price}</h3>
+    </div>
+        </div>
+        
+            `
+        }).join("");
+    }
+    else{
+       ShoppingCart.innerHTML=``;
+       label.innerHTML=`
+       <h2>Cart is Empty</h2>
+       <a href="index.html">
+        <button class="Homebtn">Back To Home</button>
+    </a>
+       `
+    }
+}
+generateCartItems();
+
+let increment = (id) => {
+    let search = basket.find((x) => x.id === id);
+    if (search === undefined) {
+        basket.push({
+            id: id,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+     generateCartItems();
+    update(id);
+    localStorage.setItem("data",JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+    let search = basket.find((x) => x.id === id);
+    if (search === undefined || search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    update(id);
+    basket=basket.filter((x)=>x.item !== 0);
+    generateCartItems();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+};
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search?.item || 0;
+    calculation();
+    TotalAmount();
+
+};
+
+let removeItem=(id)=>{
+    basket=basket.filter((x)=>x.id !== id);
+    generateCartItems();
+    TotalAmount();
+    calculation();
+     localStorage.setItem("data",JSON.stringify(basket));
+
+}
+
+let clearCart=()=>{
+    basket=[];
+    generateCartItems();
+    calculation();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+}
+
+let TotalAmount=()=>{
+    if (basket.length !== 0) {
+        let amount=basket.map((x)=>{
+            let{item,id}=x;
+            let search=shopItemsData.find((y)=>y.id === id) || [];
+            return item * search.price;
+        }).reduce((x,y)=>x+y,0);
+        // console.log(amount);
+        label.innerHTML=
+        
+        `
+        <h2>Total Bill: $ ${amount}</h2>
+        <button class="checkout">CheckOut</button>
+        <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+        `
+    }
+    else{
+        return;
+    }
+};
+
+TotalAmount();
---
+++ src/data.js
@@ -0,0 +1,58 @@
+let shopItemsData=[
+    {
+        id:"1",
+        name:"Festival Dress",
+        price:"45",
+        desc:"Trendy cotton festival dresses for stylish girls aged 18–25 — where comfort meets celebration.",
+        img:"images/img1.jpg"
+    },
+    {
+        id:"2",
+        name:"Casual Dress",
+        price:"30",
+        desc:"Chic and comfy casual dresses for girls aged 18–25 — perfect for everyday style with a youthful vibe.",
+        img:"images/img2.jpg"
+    },
+    {
+        id:"3",
+        name:"Girls Dress",
+        price:"50",
+        desc:"Stylish and versatile dresses for girls aged 18–25 — made to match every mood, moment, and move.",
+        img:"images/img6.jpg"
+    },
+    {
+        id:"4",
+        name:"Dresses For Girls",
+        price:"60",
+        desc:"Discover the latest and best 1-piece dresses — effortlessly stylish, perfectly made for 18–25 fashion-forward girls.",
+        img:"images/img4.jpg"
+    },
+      {
+        id:"5",
+        name:"Dresses For Girls",
+        price:"25",
+        desc:"Gracefully designed traditional dresses — blending timeless elegance with modern charm for today’s girls.",
+        img:"images/img5.jpeg"
+    },
+       {
+        id:"6",
+        name:"Maria B winter wedding dresses",
+        price:"55",
+        desc:"Elegant Maria B winter wedding dresses — where timeless tradition meets luxurious warmth and bridal grace. ",
+        img:"images/img3.jpeg"
+    },
+       {
+        id:"7",
+        name:"Dresses For Girls",
+        price:"40",
+        desc:"Turn heads in our stunning birthday party dresses — made to make your special day unforgettable! ",
+        img:"images/img7.jpeg"
+    },
+       {
+        id:"8",
+        name:"Dresses For Girls",
+        price:"35",
+        desc:"Beautifully crafted dresses for girls — perfect for every day, every mood, and every celebration.",
+        img:"images/img8.jpg"
+    },
+];
